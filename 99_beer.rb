52a9a5783d5c6d2d146018aeb8976c99d231f9c1
Jill Headen <jheaden@gmail.com>
2013-09-03 08:48:45 -0700

    a small collection of exercises

---
+++ 99_beer.rb
@@ -0,0 +1,7 @@
+b = 9
+
+while (b!=0)
+puts b.to_s + ' bottles of beer on the wall... etc'
+b = (b-1)
+puts 'after one bottle is consumed, ' + b.to_s + ' bottles remain on the wall.'
+end
---
+++ angry_boss.rb
@@ -0,0 +1,3 @@
+puts 'What do you want?'
+want = gets.chomp
+puts 'WHADDYA MEAN "' + want.upcase + '"!?! YOU\'RE FIRED!!!'
