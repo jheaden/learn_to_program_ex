2018e5728c9b9c35f3a91a2885f53f45ce3f8d83
Jill Headen <jheaden@gmail.com>
2013-09-02 21:37:35 -0700

    exercise 5.6

---
+++ 5_6.rb
@@ -0,0 +1,7 @@
+puts 'Hi! What\'s your first name?'
+fname = gets.chomp
+puts 'And your middle name?'
+mname = gets.chomp
+puts 'What is your last name?'
+lname = gets.chomp
+puts 'So, your whole name is ' + fname + ' ' + mname + ' ' + lname + '. Cool!'
