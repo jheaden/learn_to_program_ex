894ad8b674ac9f9fb520e884b3b630cf0a73585c
Jill Headen <jheaden@gmail.com>
2013-09-04 10:05:16 -0700

    misc. homework files

---
+++ codeacademy.rb
@@ -0,0 +1,9 @@
+puts 'Enter something: '
+name = gets.chomp
+puts name
+name2 = name.downcase
+name3 = name2.reverse
+name4 = name.reverse.upcase
+puts '2: ' + name2
+puts '3: ' + name3
+puts '4: ' + name4
