1808aa09f199a0046e1eac0d272f43976fcad072
Jill Headen <jheaden@gmail.com>
2013-09-04 10:25:10 -0700

    ersatz formatting using .ljust et al

---
+++ toc.rb
@@ -0,0 +1,10 @@
+#TOC 6.2 p 32
+lw = 55
+ch='Chapter '
+pg ='page '
+
+puts 'Table of Contents'
+puts ch + '1:   ' + 'Getting Started'.ljust(lw/2) + pg.center(lw/10) + '1'.rjust(lw/20)
+puts ch + '2:   ' + 'Numbers'.ljust(lw/2) + pg.center(lw/10) + '9'.rjust(lw/20)
+puts ch + '3:   ' + 'Letters'.ljust(lw/2) + pg.center(lw/10) + '13'.rjust(lw/20)
+puts ch + '4:   ' + 'Variables and Assignments'.ljust(lw/2) + pg.center(lw/10) + '17'.rjust(lw/20)
